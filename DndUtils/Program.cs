26a36102ec2c627745905478a055067d6bf796b7
Jordan <paynejordand@gmail.com>
2020-06-06 16:08:02 -0400

    initial commit

---
+++ DndUtils/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DndUtils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
