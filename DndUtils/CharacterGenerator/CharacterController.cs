f41f47b20539af19f92694df6a4a8c54ef896464
Jordan <paynejordand@gmail.com>
2020-07-21 13:12:48 -0400

    Added a real rough and dirty random character

--- DndUtils/CharacterGenerator/CharacterController.cs
+++ DndUtils/CharacterGenerator/CharacterController.cs
@@ -5,7 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
-
+using System.Collections.ObjectModel;
+using System.Collections;
 
 namespace DndUtils.CharacterGenerator
 {
@@ -13,6 +14,7 @@ namespace DndUtils.CharacterGenerator
     {
         private readonly CharacterModel model;
         private readonly CharacterView view;
+        private bool randomCharacter = false;
 
         public static readonly HashSet<string> allLanguages = new HashSet<string> { "Common", "Dwarvish", "Elvish", "Giant", "Gnomish",
                            "Goblin", "Halfling", "Orc", "Abyssal", "Celestial",
@@ -216,7 +218,13 @@ namespace DndUtils.CharacterGenerator
                 model.PlayerLeveling++;
                 model.PlayerRolledHealth += RollHitDie();
                 if (model.PlayerClass.ClassASILevels.Contains(model.PlayerLeveling))
-                    SpecialLevel();
+                {
+                    if (randomCharacter)
+                        RandomSpecialLevel();
+                    else
+                        SpecialLevel();
+                }
+                    
             } while (model.PlayerLeveling < model.PlayerLevel);
         }
 
@@ -376,6 +384,107 @@ namespace DndUtils.CharacterGenerator
 
             if (selectedFeat.FeatExtraEffects)
                 selectedFeat.FeatApplyExtraEffects(this.model, this.view);
+        }
+
+        // The code below here rolls a random character. There's probably a better way to do it but for now I'm gonna leave it.
+
+        public void RandomCharacter(int level, string name)
+        {
+            randomCharacter = true;
+            model.PlayerName = name;
+            IRace pRace = RandomRace();
+            model.PlayerRace = pRace;
+            RandomRaceSpecifics();
+            IClass pClass = RandomClass();
+            model.PlayerClass = pClass;
+            RandomClassSpecifics();
+            RollStats();
+            model.PlayerLevel = level;
+
+            if (model.PlayerLevel > 1)
+                GreaterLevel();
+
+            view.PrintLine($"\n\n{model}");
+        }
+
+        private IRace RandomRace()
+        {
+            var random = new Random();
+            int index = random.Next(IRace.allRaces.Count);
+            return IRace.FactoryMethod(IRace.allRaces[index].Name);
+        }
+
+        private IClass RandomClass()
+        {
+            var random = new Random();
+            int index = random.Next(IClass.allClasses.Count);
+            return IClass.FactoryMethod(IClass.allClasses[index].Name);
+        }
+
+        private void RandomRaceSpecifics()
+        {
+            var random = new Random();
+
+            if (model.PlayerRace is BaseDwarf)
+            {
+                int index = random.Next(artisanTools.Count);
+                model.PlayerProficiencies.Add(artisanTools.ElementAt(index));
+            }
+            else if (model.PlayerRace is HighElf || model.PlayerRace is BaseHuman)
+            {
+                HashSet<string> pLanguageOptions = new HashSet<string>(allLanguages);
+                pLanguageOptions.ExceptWith(model.PlayerLanguages);
+                int index = random.Next(pLanguageOptions.Count);
+                model.PlayerLanguages.Add(pLanguageOptions.ElementAt(index));
+            }
+            else if (model.PlayerRace is HalfElf)
+            {
+                HashSet<string> pSkillOptions = new HashSet<string>(allSkills);
+                pSkillOptions.ExceptWith(model.PlayerProficiencies);
+
+                int index = random.Next(pSkillOptions.Count);
+                string pSkill = pSkillOptions.ElementAt(index);
+                model.PlayerProficiencies.Add(pSkill);
+                pSkillOptions.Remove(pSkill);
+
+                index = random.Next(pSkillOptions.Count);
+                pSkill = pSkillOptions.ElementAt(index);
+                model.PlayerProficiencies.Add(pSkill);
+
+                HashSet<string> pLanguageOptions = new HashSet<string>(allLanguages);
+                pLanguageOptions.ExceptWith(model.PlayerLanguages);
+                index = random.Next(pLanguageOptions.Count);
+                model.PlayerLanguages.Add(pLanguageOptions.ElementAt(index));
+            }
+        }
+
+        private void RandomClassSpecifics()
+        {
+            var random = new Random();
+            HashSet<string> pSkillOptions = new HashSet<string>(model.PlayerClass.ClassSkillsOptions.Value);
+            pSkillOptions.ExceptWith(model.PlayerProficiencies);
+            for (int i = model.PlayerClass.ClassSkillsOptions.Key; i > 0; i--)
+            {
+                int index = random.Next(pSkillOptions.Count);
+                string pSkill = pSkillOptions.ElementAt(index);
+                model.PlayerProficiencies.Add(pSkill);
+                pSkillOptions.Remove(pSkill);
+            }
+        }
+
+        private void RandomSpecialLevel()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (KeyValuePair<string, int> kv in model.PlayerAbilityScore)
+                {
+                    if (kv.Value < 20) 
+                    {
+                        AbilityIncrease(kv.Key);
+                        break;
+                    }
+                }
+            }
         }
     }
 }
